c6ec3349ed370c3e26837b2fec04dfbfd855697c
Chhaya Verma <chhayav300@gmail.com>
2025-10-24 09:57:01 +0530

    Integrated Mixpanel to track login, logout, page views, and user edits with specific properties.

--- src/lib/mixpanel.ts
+++ src/lib/mixpanel.ts
@@ -1,10 +1,9 @@
 import mixpanel from 'mixpanel-browser';
 
-const MIXPANEL_TOKEN = '0d4bdd357e74e917fe33fe784b6363e1'; 
+const MIXPANEL_TOKEN = import.meta.env.VITE_MIXPANEL_TOKEN; 
 
 mixpanel.init(MIXPANEL_TOKEN, {
     debug: true, 
 });
 
 export default mixpanel;
-
